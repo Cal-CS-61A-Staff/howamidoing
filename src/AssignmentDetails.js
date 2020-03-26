2910a2e36e94be941de36d1ca59ffd5933a1654f
Akshit Dewan <akshit.dewan@yahoo.com>
2020-03-26 01:54:53 -0400

    slider bug fix

--- src/AssignmentDetails.js
+++ src/AssignmentDetails.js
@@ -118,7 +118,7 @@ export default function AssignmentDetails({ onLogin }) {
             ...x, Score: assignmentScores[student][assignmentIndex],
         }))
         .filter((student) => (student.Score >= rangeMin && student.Score <= rangeMax && (TA === 'All' || student.TA === TA)));
-
+    const range = [0, 100]
     const contents = (
         <>
             <div style={{ height: "40vh" }}>
@@ -211,7 +211,7 @@ export default function AssignmentDetails({ onLogin }) {
                     <Slider
                         min={0}
                         max={bins[bins.length - 1] || 0}
-                        defaultValue={[0, bins[bins.length - 1 || 0]]}
+                        value={[rangeMin, rangeMax]}
                         valueLabelDisplay="auto"
                         onChange={(_, values) => updateBins(values, setRangeMin, setRangeMax)}
                     />
