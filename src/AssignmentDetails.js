65244996f9b6b5ea60e439e92fec9c5444083fac
rahularya <rahularya@berkeley.edu>
2020-05-07 19:43:07 -0700

    minor fixes

--- src/AssignmentDetails.js
+++ src/AssignmentDetails.js
@@ -6,13 +6,12 @@ import {
     Histogram, BarSeries, withParentSize, XAxis, YAxis,
 } from "@data-ui/histogram";
 import { Dropdown, Row, Col } from "react-bootstrap";
-import { Slider } from '@material-ui/core';
+import { Slider } from "@material-ui/core";
 
-import BinSelectors from "./BinSelectors.js";
 import StudentTable from "./StudentTable.js";
 
 import { getAssignmentLookup, getAssignments } from "./loadAssignments.js";
-import { extend } from "./StudentView"
+import { extend } from "./StudentView";
 import computeTotals from "./computeTotals.js";
 
 const ResponsiveHistogram = withParentSize(({ parentWidth, parentHeight, ...rest }) => (
@@ -23,46 +22,47 @@ const ResponsiveHistogram = withParentSize(({ parentWidth, parentHeight, ...rest
     />
 ));
 
+const showScore = (score, rangeMin, rangeMax, TAToShow, TA) => (TAToShow === "All" || TAToShow === TA) && score <= rangeMax && score >= rangeMin;
+
 const extractAssignmentData = (arr, index, TA, TAs, rangeMin, rangeMax) => (
-    arr.map(scores => scores[index]).filter((score, i) =>
-        showScore(score, rangeMin, rangeMax, TA, TAs[i]))
+    arr.map(scores => scores[index]).filter(
+        (score, i) => showScore(score, rangeMin, rangeMax, TA, TAs[i]),
+    )
 );
 
-const showScore = (score, rangeMin, rangeMax, TAToShow, TA) =>
-    (TAToShow === "All" || TAToShow === TA) && score <= rangeMax && score >= rangeMin
-
 const addAssignmentTotals = (data, assignments, topics) => {
+    // eslint-disable-next-line no-param-reassign
     data = JSON.parse(JSON.stringify(data));
     for (let student = 0; student < data.length; ++student) {
-        let scores = {}
-        const header = data[student]
-        for (const title in header) {
+        let scores = {};
+        const header = data[student];
+        for (const title of header) {
             if (assignments[title]) {
                 scores[title] = header[title];
             }
         }
-        scores = extend(scores, assignments)
+        scores = extend(scores, assignments);
         const totals = computeTotals(topics, scores, false);
-        for (const assignment in totals) {
-            header[assignment] = totals[assignment]
+        for (const assignment of totals) {
+            header[assignment] = totals[assignment];
         }
     }
     return data;
-}
+};
 
 const updateBins = (value, setRangeMin, setRangeMax) => {
-    setRangeMin(value[0])
-    setRangeMax(value[1])
-}
+    setRangeMin(value[0]);
+    setRangeMax(value[1]);
+};
 
 export default function AssignmentDetails({ onLogin }) {
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
     const [assignmentIndex, setAssignmentIndex] = useState(0);
     const [header, setHeader] = useState([]);
 
     useEffect(() => {
-        $.post("/allScores").done(({ header, scores }) => {
-            setHeader(header);
+        $.post("/allScores").done(({ header: newHeader, scores }) => {
+            setHeader(newHeader);
             setData(scores.map(x => Object.fromEntries(x.map((v, i) => [header[i], v]))));
         });
     }, []);
@@ -83,20 +83,21 @@ export default function AssignmentDetails({ onLogin }) {
 
     useEffect(() => {
         setData(addAssignmentTotals(data, assignments, topics));
-        setAssignment(assignments[currentAssignmentName])
+        setAssignment(assignments[currentAssignmentName]);
     }, [data.length === 0]);
 
     const maxScore = assignment.maxScore || 0;
     const binSize = maxScore / 4;
     const defaultBins = [0, 1, 2, 3, 4, 5];
-    const bins = assignment.maxScore && assignment.maxScore !== Infinity ? _.range(0, maxScore + 0.01, binSize) : defaultBins;
+    const bins = (assignment.maxScore && assignment.maxScore !== Infinity)
+        ? _.range(0, maxScore + 0.01, binSize) : defaultBins;
 
-    const [rangeMin, setRangeMin] = useState(0)
-    const [rangeMax, setRangeMax] = useState(bins[bins.length - 1])
+    const [rangeMin, setRangeMin] = useState(0);
+    const [rangeMax, setRangeMax] = useState(bins[bins.length - 1]);
 
     useEffect(() => {
-        setRangeMax(bins[bins.length - 1])
-    }, [bins === defaultBins])
+        setRangeMax(bins[bins.length - 1]);
+    }, [bins === defaultBins]);
 
     const TAs = data
         .map(x => x.TA);
@@ -107,51 +108,55 @@ export default function AssignmentDetails({ onLogin }) {
         .map((x, student) => ({
             ...x, Score: assignmentScores[student][assignmentIndex],
         }))
-        .filter((student) => showScore(student.Score, rangeMin, rangeMax, TA, student.TA));
+        .filter(student => showScore(student.Score, rangeMin, rangeMax, TA, student.TA));
 
     const contents = (
         <>
             <div style={{ height: "40vh" }}>
-                {students.length === 0 ||
-                    <ResponsiveHistogram
-                        ariaLabel="Lab score histogram"
-                        orientation="vertical"
-                        cumulative={false}
-                        normalized
-                        valueAccessor={datum => datum}
-                        binType="numeric"
-                        binValues={bins}
-                        renderTooltip={({ datum, color }) => (
-                            <div>
-                                <strong style={{ color }}>
-                                    {datum.bin0}
-                                    {" "}
+                {students.length === 0
+                    || (
+                        <ResponsiveHistogram
+                            ariaLabel="Lab score histogram"
+                            orientation="vertical"
+                            cumulative={false}
+                            normalized
+                            valueAccessor={datum => datum}
+                            binType="numeric"
+                            binValues={bins}
+                            renderTooltip={({ datum, color }) => (
+                                <div>
+                                    <strong style={{ color }}>
+                                        {datum.bin0}
+                                        {" "}
                                     to
-                                {" "}
-                                    {datum.bin1}
-                                </strong>
-                                <div>
-                                    <strong>count </strong>
-                                    {datum.count}
+                                        {" "}
+                                        {datum.bin1}
+                                    </strong>
+                                    <div>
+                                        <strong>count </strong>
+                                        {datum.count}
+                                    </div>
+                                    <div>
+                                        <strong>cumulative </strong>
+                                        {datum.cumulative}
+                                    </div>
+                                    <div>
+                                        <strong>density </strong>
+                                        {datum.density}
+                                    </div>
                                 </div>
-                                <div>
-                                    <strong>cumulative </strong>
-                                    {datum.cumulative}
-                                </div>
-                                <div>
-                                    <strong>density </strong>
-                                    {datum.density}
-                                </div>
-                            </div>
-                        )}
-                    >
-                        <BarSeries
-                            animated
-                            rawData={extractAssignmentData(assignmentScores, assignmentIndex, TA, TAs, rangeMin, rangeMax)}
-                        />
-                        <XAxis />
-                        <YAxis />
-                    </ResponsiveHistogram>
+                            )}
+                        >
+                            <BarSeries
+                                animated
+                                rawData={extractAssignmentData(
+                                    assignmentScores, assignmentIndex, TA, TAs, rangeMin, rangeMax,
+                                )}
+                            />
+                            <XAxis />
+                            <YAxis />
+                        </ResponsiveHistogram>
+                    )
                 }
             </div>
             <Row>
@@ -203,7 +208,7 @@ export default function AssignmentDetails({ onLogin }) {
                         max={bins[bins.length - 1] || 0}
                         value={[rangeMin, rangeMax]}
                         valueLabelDisplay="auto"
-                        onChange={(_, values) => updateBins(values, setRangeMin, setRangeMax)}
+                        onChange={(__, values) => updateBins(values, setRangeMin, setRangeMax)}
                     />
                 </Col>
             </Row>
